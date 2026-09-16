85048a6112a2cf5e69685d250bda74db09e62482
Riyaz Ali <r-ahmed@cuttingedge-enterprises.in>
2026-09-16 23:22:16 +0530

    initial commit after demo mode

--- backend/index.js
+++ backend/index.js
@@ -15,6 +15,8 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 
 const NODE_ENV = process.env.NODE_ENV || 'development';
+const APP_MODE = process.env.APP_MODE || 'production';
+const IS_DEMO = APP_MODE === 'demo';
 const PORT = parseInt(process.env.PORT || '8002', 10);
 const MONGO_URL = process.env.RESTAURANT_APP_MONGO_URI || process.env.MONGO_URI;
 let DB_NAME = process.env.DB_NAME || '';
